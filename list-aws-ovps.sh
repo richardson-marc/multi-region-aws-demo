03762eaadccf3b3e5f7ceceaa8a11af5b1e9763f
Marc Richardson <marcr@blarg.net>
2019-03-25 13:39:32 -0400

    Update list-aws-ovps.sh

--- list-aws-ovps.sh
+++ list-aws-ovps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for region in `aws ec2 describe-regions --output text |cut -f3`; do aws ec2 describe-instances --region $region --query 'Reservations[*].Instances[*].[InstanceId,LaunchTime,InstanceType,Tags[?Key==`Name`].Value|[0],State.Name,PublicIpAddress,Placement.AvailabilityZone]' --output text | column -t|grep ovp; done
+for region in `aws ec2 describe-regions --output text |cut -f3`; do aws ec2 describe-instances --region $region --query 'Reservations[*].Instances[*].[InstanceId,LaunchTime,InstanceType,Tags[?Key==`Name`].Value|[0],State.Name,PublicIpAddress,Placement.AvailabilityZone]' --output text | column -t; done
