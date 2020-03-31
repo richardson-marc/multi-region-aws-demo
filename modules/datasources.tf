ab7b585677cb39ba869d290dc46b45d567a6d386
richardson-marc <marcr@blarg.net>
2020-03-30 20:48:29 -0400

    no datasource

--- modules/datasources.tf
+++
@@ -1,5 +0,0 @@
-# Gets a list of Availability Domains
-# hopefully a filter feature will be implemented against this in the future
-# for now we can trust that the list is returned in a consistent order with
-# AD-1 as the zero index
-
